567c6abf4d87a02bac6a0245c3e063c4a5208f08
Steve Hamblett <shamblett@gtsdevlan.com>
2021-02-09 07:53:31 +0000

    Issue29 update

--- test/issues/issue29.dart
+++ test/issues/issue29.dart
@@ -4,25 +4,23 @@ import 'package:test/test.dart';
 import 'package:xml2json/xml2json.dart';
 
 void main() {
-  test(
-      'Unhandled Exception: FormatException: Unrecognized string escape',
-      () {
-        const input = '''<?xml version="1.0" encoding="UTF-8"?>
-            <soapenv:Envelope xmlns:soapenv="http://schemas.xmlsoap.org/soap/envelope/">
-        <soapenv:Body>
-        <ns1:GetCommentsResponse xmlns:ns1="http://website.com/AppWS/">
-        <status>true</status>
-        <message>Succeed</message>
-        <comments>
-        <feedback>04/04/2019\$\$##+|=\€</feedback>
-        </comments>
-        </ns1:GetCommentsResponse >
-        </soapenv:Body>
-        </soapenv:Envelope>''';
+  test('Unhandled Exception: FormatException: Unrecognized string escape', () {
+    const input = '''<?xml version="1.0" encoding="UTF-8"?>
+<soapenv:Envelope xmlns:soapenv="http://schemas.xmlsoap.org/soap/envelope/">
+   <soapenv:Body>
+      <ns1:GetCommentsResponse xmlns:ns1="http://website.com/AppWS/">
+         <status>true</status>
+         <message>Succeed</message>
+         <comments>
+            <feedback>04/04/2019\$\$##+|=\\€</feedback>
+         </comments>
+      </ns1:GetCommentsResponse >
+   </soapenv:Body>
+</soapenv:Envelope>''';
 
-        final xmlParser = Xml2Json();
-        xmlParser.parse(input);
-        var jsonResponse = xmlParser.toParker();
-        print(jsonResponse);
+    final xmlParser = Xml2Json();
+    xmlParser.parse(input);
+    var jsonResponse = xmlParser.toParker();
+    print(jsonResponse);
   });
 }
