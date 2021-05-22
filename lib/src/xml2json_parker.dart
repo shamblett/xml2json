e3766f78c24aecd067153d61ec20543ec4347784
Steve Hamblett <steve.hamblett@linux.com>
2021-05-22 10:59:08 +0100

    Issue 33

--- lib/src/xml2json_parker.dart
+++ lib/src/xml2json_parker.dart
@@ -14,6 +14,7 @@ class _Xml2JsonParker {
     Map<dynamic, dynamic>? obj = objin;
     if (node is XmlElement) {
       final nodeName = '"${node.name.qualified}"';
+      // print('nodeName----------->$nodeName');
       if (obj![nodeName] is List && !obj.keys.contains(nodeName)) {
         obj[nodeName].add(<dynamic, dynamic>{});
         obj = obj[nodeName].last;
@@ -23,38 +24,7 @@ class _Xml2JsonParker {
       } else {
         if (node.children.isNotEmpty) {
           if (node.children[0] is XmlText) {
-            final sanitisedNodeData =
-                _Xml2JsonUtils.escapeTextForJson(node.children[0].text);
-            var nodeData = '"$sanitisedNodeData"';
-            if (nodeData.isEmpty) {
-              nodeData = '';
-            }
-
-            var attrs = node.attributes;
-            // 如果开始节点里有属性
-            if (attrs.isNotEmpty) {
-              var objTemp = <dynamic, dynamic>{};
-              attrs.forEach((attr) {
-                objTemp['"_${attr.name.local}"'] = '"${attr.value}"';
-              });
-              objTemp['"value"'] = nodeData;
-              if (obj[nodeName] is Map) {
-                var jsonCopy = json.decode(json.encode(obj[nodeName]));
-                obj[nodeName] = <dynamic>[jsonCopy, objTemp];
-              } else if (obj[nodeName] is List) {
-                obj[nodeName].add(objTemp);
-              } else {
-                obj[nodeName] = objTemp;
-              }
-            } else {
-              if (obj[nodeName] is String) {
-                obj[nodeName] = <dynamic>[obj[nodeName], nodeData];
-              } else if (obj[nodeName] is List) {
-                obj[nodeName].add(nodeData);
-              } else {
-                obj[nodeName] = nodeData;
-              }
-            }
+            _parseXmlTextNode(node, obj, nodeName);
           } else if (node.children[0] is XmlCDATA) {
             final sanitisedNodeData =
                 _Xml2JsonUtils.escapeTextForJson(node.children[0].text);
@@ -63,9 +33,19 @@ class _Xml2JsonParker {
               nodeData = '';
             }
             obj[nodeName] = nodeData;
+          } else if (obj[nodeName] is Map) {
+            var jsonCopy = json.decode(json.encode(obj[nodeName]));
+            obj[nodeName] = <dynamic>[jsonCopy, <dynamic, dynamic>{}];
+            obj = obj[nodeName].last;
+          } else if (obj[nodeName] is List) {
+            obj[nodeName].add(<dynamic, dynamic>{});
+            obj = obj[nodeName].last;
           } else {
             obj[nodeName] = <dynamic, dynamic>{};
             obj = obj[nodeName];
+          }
+          if (node.attributes.isNotEmpty) {
+            _parseAttrs(node, obj);
           }
         } else {
           /* No children, empty element */
@@ -83,6 +63,47 @@ class _Xml2JsonParker {
     }
 
     return obj;
+  }
+
+  /// 解析节点里的属性值
+  void _parseAttrs(dynamic node, dynamic obj) {
+    node.attributes.forEach((attr) {
+      obj!['"_${attr.name.local}"'] = '"${attr.value}"';
+    });
+  }
+
+  /// 解析XmlText节点
+  void _parseXmlTextNode(dynamic node, dynamic obj, dynamic nodeName) {
+    final sanitisedNodeData =
+        _Xml2JsonUtils.escapeTextForJson(node.children[0].text);
+    var nodeData = '"$sanitisedNodeData"';
+    // print('nodeData----------->$nodeData');
+    if (nodeData.isEmpty) {
+      nodeData = '';
+    }
+    var attrs = node.attributes;
+    // 如果开始节点里有属性
+    if (attrs.isNotEmpty) {
+      var objTemp = <dynamic, dynamic>{};
+      _parseAttrs(node, objTemp);
+      objTemp['"value"'] = nodeData;
+      if (obj[nodeName] is Map) {
+        var jsonCopy = json.decode(json.encode(obj[nodeName]));
+        obj[nodeName] = <dynamic>[jsonCopy, objTemp];
+      } else if (obj[nodeName] is List) {
+        obj[nodeName].add(objTemp);
+      } else {
+        obj[nodeName] = objTemp;
+      }
+    } else {
+      if (obj[nodeName] is String) {
+        obj[nodeName] = <dynamic>[obj[nodeName], nodeData];
+      } else if (obj[nodeName] is List) {
+        obj[nodeName].add(nodeData);
+      } else {
+        obj[nodeName] = nodeData;
+      }
+    }
   }
 
   /// Transformer function
