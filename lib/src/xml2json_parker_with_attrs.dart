46d74a74a11a268be915acd528cbdc3a9d655c63
JuneCheng <755038283@qq.com>
2021-06-04 16:40:43 +0800

    xml中某一个节点本应该是一个数组，但是如果数组中只有一个元素，就只会被解析成一个对象，所以需要把该节点指定成数组

--- lib/src/xml2json_parker_with_attrs.dart
+++ lib/src/xml2json_parker_with_attrs.dart
@@ -107,7 +107,7 @@ class _Xml2JsonParkerWithAttrs {
         obj[nodeName] = nodeData;
       }
     }
-    if ((array ?? []).contains(node.name.qualified)) {
+    if ((array ?? []).contains(node.name.qualified) && !(obj[nodeName] is List)) {
       var jsonCopy = json.decode(json.encode(obj[nodeName]));
       obj[nodeName] = <dynamic>[jsonCopy];
     }
