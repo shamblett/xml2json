f1c0bfff4a1c30e14564f095796d4838243223a5
JuneCheng <755038283@qq.com>
2021-06-07 09:17:59 +0800

    xml中某一个节点本应该是一个数组，但是如果数组中只有一个元素，就只会被解析成一个对象，所以需要把该节点指定成数组

--- example/xml2json.dart
+++ example/xml2json.dart
@@ -38,10 +38,20 @@ void main() {
   print('Parker');
   print('');
   print(json);
+  print('');
 
   // Transform to JSON using ParkerWithAttrs
   json = myTransformer.toParkerWithAttrs();
   print('ParkerWithAttrs');
+  print('');
+  print(json);
+  print('');
+
+  // Transform to JSON using ParkerWithAttrs
+  // A node in XML should be an array, but if there is only one element in the array, it will only be parsed into an object, so we need to specify the node as an array
+  // xml中某一个节点本应该是一个数组，但是如果数组中只有一个元素，就只会被解析成一个对象，所以需要把该节点指定成数组
+  json = myTransformer.toParkerWithAttrs(array: ['contact']);
+  print('ParkerWithAttrs, specify the node as an array');
   print('');
   print(json);
 }
