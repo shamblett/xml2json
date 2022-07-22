e212c0de0498291567bdd97eaba87f8436d9a465
Steve Hamblett <shamblett@gtsdevlan.com>
2022-07-22 10:45:26 +0100

    Issue 48

---
+++ test/issues/issue48.dart
@@ -0,0 +1,29 @@
+@TestOn('vm')
+
+import 'dart:convert';
+import 'package:test/test.dart';
+import 'package:xml2json/xml2json.dart';
+
+void main() {
+  test('Incorrect transform', () {
+    const input = "<SubItems>"
+        " <Item>"
+        " <Value>1</Value>"
+        " <LinkTo>1000029214</LinkTo>"
+        " <Value>2</Value>"
+        " <LinkTo>1000029214</LinkTo>"
+        " <Value></Value>"
+        " <LinkTo>1000029214</LinkTo>"
+        " <Value>3</Value>"
+        " <LinkTo>1000029214</LinkTo>"
+        " </Item>"
+        "</SubItems>";
+
+    final xmlParser = Xml2Json();
+    xmlParser.parse(input);
+    var jsonResponse = jsonDecode(xmlParser.toParkerWithAttrs());
+    print(jsonResponse);
+    expect(jsonResponse,
+        '{SubItems: {Item": {Value": ["1","2","","3"],"LinkTo": ["1000029214","1000029214","1000029214","1000029214"],}}}');
+  });
+}
