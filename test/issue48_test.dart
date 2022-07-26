c217f66c3fb7b09f1c9261d23de0b5053be3eac3
Steve Hamblett <steve.hamblett@linux.com>
2022-07-26 09:35:02 +0100

    Issue48 (#49)
    * Issue 48
    
    * Issue 48
    
    * Issue 48
    
    * issue 48 - blank entry causing fault
    
    * Issue 48 - fixed with test
    
    Co-authored-by: Steve Hamblett <shamblett@gtsdevlan.com>

---
+++ test/issue48_test.dart
@@ -0,0 +1,28 @@
+@TestOn('vm')
+
+import 'package:test/test.dart';
+import 'package:xml2json/xml2json.dart';
+
+void main() {
+  test('Incorrect transform', () {
+    const input = "<SubItems>"
+        " <Item>"
+        " <Value>1</Value>"
+        " <LinkTo>1000029214</LinkTo>"
+        " <Value>2</Value>"
+        " <LinkTo>1000029214</LinkTo>"
+        " <Value></Value>"
+        " <LinkTo>1000029214</LinkTo>"
+        " <Value>3</Value>"
+        " <LinkTo>1000029214</LinkTo>"
+        " </Item>"
+        "</SubItems>";
+
+    final xmlParser = Xml2Json();
+    xmlParser.parse(input);
+    var jsonResponse = xmlParser.toParkerWithAttrs();
+    print(jsonResponse);
+    expect(jsonResponse,
+        '{"SubItems": {"Item": {"Value": ["1", "2", "", "3"], "LinkTo": ["1000029214", "1000029214", "1000029214", "1000029214"]}}}');
+  });
+}
