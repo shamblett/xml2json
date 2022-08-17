9059c938a7c289df5dc25e529d79e54bbeaca7fa
Steve Hamblett <shamblett@gtsdevlan.com>
2022-08-17 11:24:24 +0100

    Issue 50

--- test/issues/issue50/issue50_test.dart
+++ test/issues/issue50/issue50_test.dart
@@ -2,13 +2,17 @@
 
 import 'package:test/test.dart';
 import 'package:xml2json/xml2json.dart';
+import 'package:path/path.dart' as p;
 import 'dart:io';
 
 void main() {
   test('Book transform', () {
-
+    var filePath =
+        p.join(Directory.current.path, 'test', 'issues', 'issue50', 'book.xml');
+    File file = File(filePath);
+    final fileContent = file.readAsStringSync();
     final xmlParser = Xml2Json();
-    xmlParser.parse(input);
+    xmlParser.parse(fileContent);
     var jsonResponse = xmlParser.toParkerWithAttrs();
     print(jsonResponse);
   });
