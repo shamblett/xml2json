fd9929f047bb248e474c5fa067d48bb74054df59
Steve Hamblett <steve.hamblett@linux.com>
2023-02-14 14:02:54 +0000

    Issue54

---
+++ test/issues/issue54/issue54.dart
@@ -0,0 +1,35 @@
+@TestOn('vm')
+import 'dart:convert';
+import 'dart:io';
+
+import 'package:test/test.dart';
+import 'package:xml2json/xml2json.dart';
+
+void main() async {
+  Directory parkerAssetsDirectory = Directory("test/issues/issue54/unittests");
+  Iterable<File> xmlFiles = (await parkerAssetsDirectory
+          .list()
+          .where((event) => event is File)
+          .toList())
+      .cast<File>()
+      .where((element) => element.path.endsWith("xml"));
+
+  await Future.forEach(xmlFiles, testXmlFile);
+}
+
+Future<void> testXmlFile(File xmlFile) async {
+  String xmlFilePath = xmlFile.path;
+  String jsonFilePath =
+      "${xmlFilePath.substring(0, xmlFilePath.length - 3)}json.expected";
+  File jsonFile = File(jsonFilePath);
+  String jsonFileContent = await jsonFile.readAsString();
+  String xmlFileContent = await xmlFile.readAsString();
+
+  test("correct parker conversion of $xmlFilePath", () {
+    final xmlParser = Xml2Json();
+    xmlParser.parse(xmlFileContent);
+    var jsonResponse = xmlParser.toParker();
+    //print(jsonResponse);
+    expect(jsonDecode(jsonResponse), jsonDecode(jsonFileContent));
+  });
+}
