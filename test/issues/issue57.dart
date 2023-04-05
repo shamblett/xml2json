cd7ad47b08c591284d52a1ca1a462546548d2b95
shamblett <steve.hamblett@linux.com>
2023-04-05 13:51:05 +0100

    Issue 57

--- test/issues/issue57.dart
+++ test/issues/issue57.dart
@@ -1,16 +1,22 @@
 @TestOn('vm')
 
+import 'dart:convert';
 import 'package:test/test.dart';
 import 'package:xml2json/xml2json.dart';
 
 void main() {
   test('Multiline strings', () {
-    final s1 = '''<a> This is a 
-    multi-line string 
-    </a>''';
+    final s1 = r'''<a>This is a
+    multi-line
+    string </a>''';
     final Xml2Json myTransformer = Xml2Json();
     myTransformer.parse(s1);
+    print(s1);
     final res = myTransformer.toParker();
-    expect(res, '{"a": ```This is a multi-line string "}');
+    print(res);
+    final decoder = JsonDecoder();
+    final decoded = decoder.convert(res);
+    print(decoded);
+    expect(decoded.toString(), r'{a: This is a\\n    multi-line\\n    string }');
   });
 }
