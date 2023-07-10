f98f0a6b7d9e7ee614e6e2f6adf55c0cfdb2f69b
Steve Hamblett <steve.hamblett@linux.com>
2023-07-10 10:59:54 +0100

    Issue 23 reopened

--- test/issue23_test.dart
+++ test/issue23_test.dart
@@ -20,40 +20,40 @@ void main() {
 
   test(
       'xml2json escapes &quot; in the json output when used in an xml attribute - OpenRally',
-          () {
-        final xml = '<element att=" &quot;test&quot; "/>';
-        print(xml);
-        final actual = (Xml2Json()..parse(xml)).toOpenRally();
-        print(actual);
-        final expected = '{"element": {"att": " \\"test\\" "}}';
-        expect(actual, equals(expected));
-        final decoded = json.decode(actual);
-        expect(decoded['element']['att'], ' "test" ');
-      },skip:true);
+      () {
+    final xml = '<element att=" &quot;test&quot; "/>';
+    print(xml);
+    final actual = (Xml2Json()..parse(xml)).toOpenRally();
+    print(actual);
+    final expected = '{"element": {"att": " \\"test\\" "}}';
+    expect(actual, equals(expected));
+    final decoded = json.decode(actual);
+    expect(decoded['element']['att'], ' "test" ');
+  }, skip: true);
 
   test(
       'xml2json escapes &quot; in the json output when used in an xml attribute - Parker',
-          () {
-        final xml = '<element att=" &quot;test&quot; "/>';
-        print(xml);
-        final actual = (Xml2Json()..parse(xml)).toParker();
-        print(actual);
-        final expected = '{"element": {"@att": " \\"test\\" "}}';
-        expect(actual, equals(expected));
-        final decoded = json.decode(actual);
-        expect(decoded['element']['@att'], ' "test" ');
-      },skip:true);
+      () {
+    final xml = '<element att=" &quot;test&quot; "/>';
+    print(xml);
+    final actual = (Xml2Json()..parse(xml)).toParker();
+    print(actual);
+    final expected = '{"element": {"@att": " \\"test\\" "}}';
+    expect(actual, equals(expected));
+    final decoded = json.decode(actual);
+    expect(decoded['element']['@att'], ' "test" ');
+  }, skip: true);
 
   test(
       'xml2json escapes &quot; in the json output when used in an xml attribute - GData',
-          () {
-        final xml = '<element att=" &quot;test&quot; "/>';
-        print(xml);
-        final actual = (Xml2Json()..parse(xml)).toParker();
-        print(actual);
-        final expected = '{"element": {"@att": " \\"test\\" "}}';
-        expect(actual, equals(expected));
-        final decoded = json.decode(actual);
-        expect(decoded['element']['@att'], ' "test" ');
-      },skip:true);
+      () {
+    final xml = '<element att=" &quot;test&quot; "/>';
+    print(xml);
+    final actual = (Xml2Json()..parse(xml)).toParker();
+    print(actual);
+    final expected = '{"element": {"@att": " \\"test\\" "}}';
+    expect(actual, equals(expected));
+    final decoded = json.decode(actual);
+    expect(decoded['element']['@att'], ' "test" ');
+  }, skip: true);
 }
