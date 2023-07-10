24fc9c050ab101695c4e3e591bbe9ebf6086ea78
Steve Hamblett <steve.hamblett@linux.com>
2023-07-10 10:57:36 +0100

    Issue 23 reopened

--- test/issue23_test.dart
+++ test/issue23_test.dart
@@ -6,7 +6,7 @@ import 'package:xml2json/xml2json.dart';
 
 void main() {
   test(
-      'xml2json escapes &quot; in the json output when used in an xml attribute',
+      'xml2json escapes &quot; in the json output when used in an xml attribute - Badgerfish',
       () {
     final xml = '<element att=" &quot;test&quot; "/>';
     print(xml);
@@ -17,4 +17,43 @@ void main() {
     final decoded = json.decode(actual);
     expect(decoded['element']['@att'], ' "test" ');
   });
+
+  test(
+      'xml2json escapes &quot; in the json output when used in an xml attribute - OpenRally',
+          () {
+        final xml = '<element att=" &quot;test&quot; "/>';
+        print(xml);
+        final actual = (Xml2Json()..parse(xml)).toOpenRally();
+        print(actual);
+        final expected = '{"element": {"att": " \\"test\\" "}}';
+        expect(actual, equals(expected));
+        final decoded = json.decode(actual);
+        expect(decoded['element']['att'], ' "test" ');
+      },skip:true);
+
+  test(
+      'xml2json escapes &quot; in the json output when used in an xml attribute - Parker',
+          () {
+        final xml = '<element att=" &quot;test&quot; "/>';
+        print(xml);
+        final actual = (Xml2Json()..parse(xml)).toParker();
+        print(actual);
+        final expected = '{"element": {"@att": " \\"test\\" "}}';
+        expect(actual, equals(expected));
+        final decoded = json.decode(actual);
+        expect(decoded['element']['@att'], ' "test" ');
+      },skip:true);
+
+  test(
+      'xml2json escapes &quot; in the json output when used in an xml attribute - GData',
+          () {
+        final xml = '<element att=" &quot;test&quot; "/>';
+        print(xml);
+        final actual = (Xml2Json()..parse(xml)).toParker();
+        print(actual);
+        final expected = '{"element": {"@att": " \\"test\\" "}}';
+        expect(actual, equals(expected));
+        final decoded = json.decode(actual);
+        expect(decoded['element']['@att'], ' "test" ');
+      },skip:true);
 }
